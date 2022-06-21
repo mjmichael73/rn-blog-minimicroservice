3a13d24b715e3b89f334d03f656b6d3ecb0a3e3f
Mojtaba Michael <mojimich2015@gmail.com>
2022-06-21 22:02:44 +0430

    It was completed posts-depl.yaml file

--- posts/index.js
+++ posts/index.js
@@ -36,10 +36,10 @@ app.post("/posts", async (req, res) => {
 
 app.post("/events", (req, res) => {
   console.log("Received Event", req.body.type);
-
   res.send({});
 });
 
 app.listen(4000, () => {
+  console.log("Version 2");
   console.log("Listening on 4000");
 });
